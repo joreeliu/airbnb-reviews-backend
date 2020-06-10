d23d3288aad21ca786ce6d22e660f9869676573c
yy2792 <yy2792@columbia.edu>
2020-06-10 15:51:04 -0400

    updated for deploy

--- application.py
+++ application.py
@@ -15,6 +15,32 @@ CORS(app)
 engine = create_engine(URL(**app.config['DATABASE']))
 
 
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# EB looks for an 'application' callable by default.
+app = Flask(__name__)
+
+# add a rule for the index page.
+app.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+app.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
+
+
 @app.route('/get_neighborhood/')
 def get_neighborhood():
     qry = """select * from dbo.neighbourhoods"""
@@ -158,4 +184,5 @@ def get_neighborhood_score(neighborhood):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host="0.0.0.0")
